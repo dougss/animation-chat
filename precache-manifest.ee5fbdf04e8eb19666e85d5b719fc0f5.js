7d1819b3e392e5064aa9e4f21e2876098b966416
Douglas Silva <doug_ss@live.com>
2020-07-20 03:29:44 -0300

    Updates

---
+++ precache-manifest.ee5fbdf04e8eb19666e85d5b719fc0f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940f0226dec4442bb43560b48d73e93a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "249b428ea9b1bb96e5b6",
+    "url": "/static/css/main.760db27f.chunk.css"
+  },
+  {
+    "revision": "2d3ec9e95745111f002c",
+    "url": "/static/js/2.bb02b7c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb02b7c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249b428ea9b1bb96e5b6",
+    "url": "/static/js/main.58ef0953.chunk.js"
+  },
+  {
+    "revision": "14746a0a704e77d0a74d",
+    "url": "/static/js/runtime-main.66eb8df2.js"
+  }
+]);
