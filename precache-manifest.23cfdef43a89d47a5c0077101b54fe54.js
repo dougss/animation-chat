cdf65ddb64ada37c9f1891a9d00d67ab35e74d52
Douglas Silva <doug_ss@live.com>
2020-07-20 03:40:35 -0300

    Updates

---
+++ precache-manifest.23cfdef43a89d47a5c0077101b54fe54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d65af09fb525580d550d2b4ea7988b",
+    "url": "/animation-chat/index.html"
+  },
+  {
+    "revision": "542599e84a7d6742fb76",
+    "url": "/animation-chat/static/css/main.760db27f.chunk.css"
+  },
+  {
+    "revision": "8be14487ae137d3ae573",
+    "url": "/animation-chat/static/js/2.f401d066.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/animation-chat/static/js/2.f401d066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542599e84a7d6742fb76",
+    "url": "/animation-chat/static/js/main.3988c8f0.chunk.js"
+  },
+  {
+    "revision": "e1d343756cbee1d304a9",
+    "url": "/animation-chat/static/js/runtime-main.94713a07.js"
+  }
+]);
